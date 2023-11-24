5c109f61c671c08d0dd807d49a73ed941287d226
AlgoWebBot <shimulzahan636@gmail.com>
2023-11-24 23:02:24 +0600

    contact page update

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,29 +1,144 @@
 import React from 'react'
 import icon from '../../assets/icons/group.png'
+import TitleBar from '../../Utils/TitleBar'
 
 const Contact = () => {
     return (
         <div className='mt-16'>
-            <section className="bg-white dark:bg-gray-900">
-                <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-                    <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center dark:text-white">Contact Us</h2>
-                    <p className="mb-8 lg:mb-16 font-light text-center text-black dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
-                    <form action="#" className="space-y-8">
-                        <div>
-                            <label for="email" className="block mb-2 text-lg font-medium text-gray-900 dark:text-gray-300">Your email</label>
-                            <input type="email" id="email" className="shadow-sm text-lg bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="algowebbot@gmail.com" required />
+            <section class="bg-gray-100 lg:mb-20">
+                <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+                    <TitleBar class="mt-20" title={'CONTACT WITH US'} />
+                    <div class="grid grid-cols-1 gap-x-16 gap-y-8 lg:grid-cols-5">
+                        <div class="lg:col-span-2 lg:py-12">
+                            <p class="max-w-xl text-lg">
+                                At the same time, the fact that we are wholly owned and totally
+                                independent from manufacturer and other group control gives you
+                                confidence that we will only recommend what is right for you.
+                            </p>
+
+                            <div class="mt-8">
+                                <a href="" class="text-2xl font-bold text-pink-600">
+                                    0151 475 4450
+                                </a>
+
+                                <address class="mt-2 not-italic">
+                                    282 Kevin Brook, Imogeneborough, CA 58517
+                                </address>
+                            </div>
                         </div>
-                        <div>
-                            <label for="subject" className="block mb-2 text-lg font-medium text-gray-900 dark:text-gray-300">Subject</label>
-                            <input type="text" id="subject" className="block p-3 w-full text-lg  text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Let us know how can we help you" required />
+
+                        <div class="rounded-lg bg-white p-8 shadow-lg lg:col-span-3 lg:p-12">
+                            <form action="" class="space-y-4">
+                                <div>
+                                    <label class="sr-only" for="name">Name</label>
+                                    <input
+                                        class="w-full rounded-lg border-gray-200 p-3 text-sm"
+                                        placeholder="Name"
+                                        type="text"
+                                        id="name"
+                                    />
+                                </div>
+
+                                <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
+                                    <div>
+                                        <label class="sr-only" for="email">Email</label>
+                                        <input
+                                            class="w-full rounded-lg border-gray-200 p-3 text-sm"
+                                            placeholder="Email address"
+                                            type="email"
+                                            id="email"
+                                        />
+                                    </div>
+
+                                    <div>
+                                        <label class="sr-only" for="phone">Phone</label>
+                                        <input
+                                            class="w-full rounded-lg border-gray-200 p-3 text-sm"
+                                            placeholder="Phone Number"
+                                            type="tel"
+                                            id="phone"
+                                        />
+                                    </div>
+                                </div>
+
+                                <div class="grid grid-cols-1 gap-4 text-center sm:grid-cols-3">
+                                    <div>
+                                        <input
+                                            class="peer sr-only"
+                                            id="option1"
+                                            type="radio"
+                                            tabindex="-1"
+                                            name="option"
+                                        />
+
+                                        <label
+                                            for="option1"
+                                            class="block w-full rounded-lg border border-gray-200 p-3 text-gray-600 hover:border-black peer-checked:border-black peer-checked:bg-black peer-checked:text-white"
+                                            tabindex="0"
+                                        >
+                                            <span class="text-sm"> Option 1 </span>
+                                        </label>
+                                    </div>
+
+                                    <div>
+                                        <input
+                                            class="peer sr-only"
+                                            id="option2"
+                                            type="radio"
+                                            tabindex="-1"
+                                            name="option"
+                                        />
+
+                                        <label
+                                            for="option2"
+                                            class="block w-full rounded-lg border border-gray-200 p-3 text-gray-600 hover:border-black peer-checked:border-black peer-checked:bg-black peer-checked:text-white"
+                                            tabindex="0"
+                                        >
+                                            <span class="text-sm"> Option 2 </span>
+                                        </label>
+                                    </div>
+
+                                    <div>
+                                        <input
+                                            class="peer sr-only"
+                                            id="option3"
+                                            type="radio"
+                                            tabindex="-1"
+                                            name="option"
+                                        />
+
+                                        <label
+                                            for="option3"
+                                            class="block w-full rounded-lg border border-gray-200 p-3 text-gray-600 hover:border-black peer-checked:border-black peer-checked:bg-black peer-checked:text-white"
+                                            tabindex="0"
+                                        >
+                                            <span class="text-sm"> Option 3 </span>
+                                        </label>
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <label class="sr-only" for="message">Message</label>
+
+                                    <textarea
+                                        class="w-full rounded-lg border-gray-200 p-3 text-sm"
+                                        placeholder="Message"
+                                        rows="8"
+                                        id="message"
+                                    ></textarea>
+                                </div>
+
+                                <div class="mt-4">
+                                    <button
+                                        type="submit"
+                                        class="inline-block w-full rounded-lg bg-black px-5 py-3 font-medium text-white sm:w-auto"
+                                    >
+                                        Send Enquiry
+                                    </button>
+                                </div>
+                            </form>
                         </div>
-                        <div className="sm:col-span-2">
-                            <label for="message" className="block mb-2 text-lg font-medium text-gray-900 dark:text-gray-400">Your message</label>
-                            <textarea id="message" rows="6" className="block p-2.5 w-full text-lg text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
-                        </div>
-                        <button type="submit" className="py- px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
-                    </form>
-                    <button className='btn bg-[#1976D2] rounded-sm py-2 text-lg px-10 capitalize font-light'>Send</button>
+                    </div>
                 </div>
             </section>
 
