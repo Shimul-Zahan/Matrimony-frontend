5f4a9afc71a754b9b508fff6f865e8570e80ab84
AlgoWebBot <shimulzahan636@gmail.com>
2023-11-25 20:17:03 +0600

    user premium request update

--- src/Pages/DashboardPages/User/ViewBio.jsx
+++ src/Pages/DashboardPages/User/ViewBio.jsx
@@ -8,12 +8,15 @@ const ViewBio = () => {
 
     const { data, refetch, isLoading } = useBioDetailsDash();
     const axiosSecureInstance = useAxiosSecure();
-    // console.log(data)
+    console.log(data)
 
     const requestedData = {
-        name: data?.name,
-        email: data?.userEmail,
-        biodataId: data?.biodataId
+        oldId: data?._id,
+        ...data
+    }
+
+    if (isLoading) {
+        return <div>Loading....</div>
     }
 
     const requestPremium = () => {
@@ -27,13 +30,17 @@ const ViewBio = () => {
             confirmButtonText: "Yes, request for it!"
         }).then((result) => {
             if (result.isConfirmed) {
-                axiosSecureInstance.post(`/premium-request/${data._id}`)
-                console.log(requestedData)
-                Swal.fire({
-                    title: "Request Send!",
-                    text: "Successfully requested for premium membership!.",
-                    icon: "success"
-                });
+                axiosSecureInstance.post('/premium-request', requestedData)
+                    .then(res => {
+                        if (res.data.acknowledged) {
+                            Swal.fire({
+                                title: "Request Send!",
+                                text: "Successfully requested for premium membership!.",
+                                icon: "success"
+                            });
+                        }
+                        refetch();
+                    })
             }
         });
     }
@@ -54,11 +61,11 @@ const ViewBio = () => {
                     </div>
 
                     <div className="mt-20 text-center pb-6">
-                        <h1 className="text-4xl font-medium text-indigo-500">{data?.name}, <span className="font-light text-gray-500">{ data?.age }</span></h1>
-                        <p className="mt-4 text-gray-500 capitalize">{ data?.occupation }</p>
+                        <h1 className="text-4xl font-medium text-indigo-500">{data?.name}, <span className="font-light text-gray-500">{data?.age}</span></h1>
+                        <p className="mt-4 text-gray-500 capitalize">{data?.occupation}</p>
                         <div className='flex flex-col lg:flex-row justify-center items-center gap-5'>
                             <p className="font-light text-gray-600 mt-3 capitalize">
-                                <span className='text-black'>Present Division:</span> { data?.presentDivision }</p>
+                                <span className='text-black'>Present Division:</span> {data?.presentDivision}</p>
                             <p className="font-light text-gray-600 mt-3">
                                 <span className='text-black'>Permanent Division:</span> {data?.permanentDivision}</p>
                         </div>
@@ -73,7 +80,7 @@ const ViewBio = () => {
                     </span>
 
                     <div className="mt-2 flex flex-col justify-center">
-                        
+
                         <div className='flex flex-col lg:flex-row justify-center items-center gap-5'>
                             <p className="font-light text-gray-600 mt-3">
                                 <span className='text-black'>BiodataId:</span> {data?.biodataId}
@@ -86,7 +93,7 @@ const ViewBio = () => {
                             </p>
 
                         </div>
-                        
+
                         <div className='flex flex-col lg:flex-row justify-center items-center gap-5 mb-3'>
                             <p className="font-light text-gray-600 mt-3 capitalize">
                                 <span className='text-black'>Height:</span> {data?.height}
@@ -97,7 +104,7 @@ const ViewBio = () => {
                             <p className="font-light text-gray-600 mt-3">
                                 <span className='text-black'>Race:</span> {data?.race}
                             </p>
-                            
+
                         </div>
 
                         <span className="relative flex justify-center">
@@ -124,7 +131,7 @@ const ViewBio = () => {
 
                             <span className="relative z-10 bg-white px-6 tex-lg">Expected Partner Details</span>
                         </span>
-                    
+
                         <div className='flex flex-col lg:flex-row justify-center items-center gap-5 mb-3'>
                             <p className="font-light text-gray-600 mt-3 capitalize">
                                 <span className='text-black'>Age:</span> {data?.expectedPartnerAge}
@@ -136,11 +143,19 @@ const ViewBio = () => {
                                 <span className='text-black'>Height:</span> {data?.expectedPartnerHeight}
                             </p>
                         </div>
-                        <button onClick={requestPremium}
-                            className="bg-indigo-500 py-2 px-4 w-2/5 mx-auto rounded-sm text-white font-medium mt-4"
-                        >
-                            Make biodata to premium
-                        </button>
+                        {
+                            data?.premiumRequestStatus === 'pending' ?
+                                <button disabled
+                                    className="bg-indigo-500 py-2 px-4 w-2/5 mx-auto rounded-sm text-white font-medium mt-4"
+                                >
+                                    Request Pending
+                                </button> :
+                                <button onClick={requestPremium}
+                                    className="bg-indigo-500 py-2 px-4 w-2/5 mx-auto rounded-sm text-white font-medium mt-4"
+                                >
+                                    Make biodata to premium
+                                </button>
+                        }
                     </div>
 
                 </div>
