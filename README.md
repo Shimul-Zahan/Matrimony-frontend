77ed2aa60524cdee07746db1e140b7a1a1b6896b
AlgoWebBot <shimulzahan636@gmail.com>
2023-12-09 15:13:48 +0600

    update readme file

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
+# website feature
+
+It is a social matrimony site. This is a fully dynamic site.
+Where this site of mine will work on the layout of two completely different users. Like here a user can create his profile and find the boy/girl of his choice. He can become a premium member if he wants and if he is not a premium member then he will not be able to see the contract information of a boy/girl for which he will have to request 500 rupees.
+On the other hand, let's come to the admin dashboard where an admin can add, delete, premium members.
+Besides, contact information request, premium request except left can be deleted.
+I have given privacy to all funds of this site. Here you will find two different dashboards.
+
+Use Technologics
+.Tailwind CSS .Javascript .React Js .Node Js .Express Js .MongoDB .Stripe .Firebase .Jwt .Mui .Axios .Tanstack Query
+
+Web Tools
+.Git .Github .Surge .Vervcel
+
+# Name : Memory Matrimony BD
 # Live Site Link: https://memory-matrimony-bd.surge.sh/
--- src/Pages/DashboardPages/Admin/ManageUsers.jsx
+++ src/Pages/DashboardPages/Admin/ManageUsers.jsx
@@ -16,7 +16,7 @@ const ManageUsers = () => {
 
     useEffect(() => {
         setDisplayData(data);
-    }, [isLoading, refetch()])
+    }, [isLoading])
 
     if (isLoading) {
         return <Loading />
@@ -75,7 +75,7 @@ const ManageUsers = () => {
         });
     }
 
-    const handelSearch = async e => {
+    const handelSearch = async (e) => {
         e.preventDefault();
         const search = e.target.search.value;
         const res = await axiosSecureInstance.get(`/search-manage-users?name=${search}`)
@@ -161,15 +161,15 @@ const ManageUsers = () => {
                 </table>
                 <div className='fixed px-2 bottom-3 w-full container lg:w-[1000px]'>
                     <form onSubmit={handelSearch}>
-                        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-                        <div class="relative">
-                            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <label for="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                        <div className="relative">
+                            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                                <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                                 </svg>
                             </div>
-                            <input type="search" name='search' class="block w-full p-4 ps-10 text-lg text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500" required />
-                                <button type="submit" class="text-white px-8 absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+                            <input type="search" name='search' className="block w-full p-4 ps-10 text-lg text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500" required />
+                            <button type="submit" className="text-white px-8 absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
                         </div>
                     </form>
                 </div>
